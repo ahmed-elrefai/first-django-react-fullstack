a7d37f8c58032447eddc53758c977d0ae05db166
Ahmed Elrefai <114250600+ahmed-elrefai@users.noreply.github.com>
2024-06-08 03:07:14 +0300

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,10 @@
 import axios from "axios"
 import { ACCESS_TOKEN} from "./constants"
 
+const apiUrl = "/choreo-apis/djangoreactfullstack/backend/v1"
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_APP_URL
+    baseURL: import.meta.env.VITE_APP_URL ? import.meta.env.VITE_APP_URL : apiUrl,
 })
 
 api.interceptors.request.use(
